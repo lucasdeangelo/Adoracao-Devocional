953cf1372f6a9750c8778adb8cfd9af615368119
Lucas de Angelo <lucasdeangelo343@gmail.com>
2025-08-10 16:14:09 -0300

    Edit - Estilização de tag de links

--- src/app/articles/[slug]/page.js
+++ src/app/articles/[slug]/page.js
@@ -35,6 +35,7 @@ export default async function ArticlePage({ params }) {
               h1: ({ node, ...props }) => <h1 className="text-3xl  font-bold mb-4" {...props} />,
               h2: ({ node, ...props }) => <h2 className="text-xl  font-bold mb-4" {...props} />,
               h3: ({ node, ...props }) => <h3 className="text-lg  font-bold mb-4" {...props} />,
+              a: ({ node, ...props }) => <h3 className="text-lg  font-bold mb-4 cursor-pointer hover:underline" {...props} />,
               p: ({ node, ...props }) => <p className=" my-6 leading-relaxed" {...props} />, 
               ul: ({ node, ...props }) => <ul className=" list-disc font-medium mb-4 ml-4" {...props} />,
               li: ({ node, ...props }) => <li className=" list-decimal font-medium mb-4 ml-4" {...props} />,
